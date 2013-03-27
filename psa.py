5ab5cf39c01ea83a4f7ec77c5b5fdce63c57a4ff
Gareth Foster <shenlok@gmail.com>
2013-03-27 17:43:27 +0000

    Initial implementations of NoisyEnc and AggrDec

--- psa.py
+++ psa.py
@@ -1,6 +1,7 @@
 from viff.field import GF
 from viff.util import find_prime
 from gmpy import mpz
+from operator import mul
 import random
 
 
@@ -13,21 +14,26 @@ class PSA:
         return self._hashes[x]
 
     def NoisyEnc(self, param, sk, t, xbar):
-        pass
+        c = (g^xbar) * self._H(t)^sk
+        return c
         
+    def AggrDec(self, param, sk, t, cs):
+        cprod = reduce(mul, cs, 1) # Get the product of all the ciphertexts
+        v = (self._H(t)^sk) * cprod
+        return v
 
 
     # k is the security parameter
     def setup(self, k, n):
         q, p = self._find_p(k) 
         Zp = GF(p)
-        g = self._find_gen(Zp, q)
-        sks = [0] * (n + 1)
+        self.g = self._find_gen(Zp, q)
+        self.sks = [0] * (n + 1)
         for i in range(1, n + 1):
-            sks[i] = Zp(random.randrange(Zp.modulus))   # Here I replaced Zp.random_element() as this function doesn't seem to exist on FieldElement's
-        sks[0] = -sum(sks[1:])
+            self.sks[i] = Zp(random.randrange(Zp.modulus))   # Here I replaced Zp.random_element() as this function doesn't seem to exist on FieldElement's
+        self.sks[0] = -sum(self.sks[1:])
 
-        return (g, sks)
+        return (self.g, self.sks)
 
     def _find_p(self, k):
         q = find_random_prime(k)
